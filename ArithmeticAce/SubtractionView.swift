2983fa9de9979f40e129f94596fabcb6ca5e2f36
matt collyer <mcollyer@lcs.on.ca>
2022-02-11 15:23:40 -0500

    Added three new equation style pages. Division, subtraction and addition.

---
+++ ArithmeticAce/SubtractionView.swift
@@ -0,0 +1,136 @@
+//
+//  SubtractionView.swift
+//  ArithmeticAce
+//
+//  Created by Russell Gordon on 2022-02-07.
+//
+
+import SwiftUI
+
+struct SubtractionView: View {
+    
+    // MARK: Stored properties
+    @State var minuend = Int.random(in: 1...12)
+    @State var subtrahend = Int.random(in: 1...12)
+    @State var inputGiven = ""
+    
+    // Has an answer been checked?
+    @State var answerChecked = false
+    
+    // Was the answer given actually correct?
+    @State var answerCorrect = false
+    
+    // MARK: Computed properties
+    // What is the correct product?
+    var correctSum: Int {
+        return minuend - subtrahend
+    }
+    
+    var body: some View {
+        
+        VStack(spacing: 0) {
+            HStack {
+                Text("-")
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text("\(minuend)")
+                    Text("\(subtrahend)")
+                }
+            }
+            
+            Divider()
+            
+            HStack {
+                ZStack{
+                    Image(systemName: "checkmark.circle")
+                        .foregroundColor(.green)
+                    //        CONDITION      true  false
+                        .opacity(answerCorrect ? 1.0 : 0.0)
+                    
+                    Image(systemName: "x.square")
+                        .foregroundColor(.red)
+                        .opacity(answerCorrect == false && answerChecked == false ? 0.0 : 1.0)
+                    
+                    
+                }
+                Spacer()
+                TextField("",
+                          text: $inputGiven)
+                    .multilineTextAlignment(.trailing)
+            }
+          
+            ZStack{
+                
+            Button(action: {
+                
+                // Answer has been checked!
+                answerChecked = true
+                
+                // Convert the input given to an integer, if possible
+                guard let productGiven = Int(inputGiven) else {
+                    // Sadness, not a number
+                    answerCorrect = false
+                    return
+                }
+                
+                // Check the answer!
+                if productGiven == correctSum {
+                    // Celebrate! 👍🏼
+                    answerCorrect = true
+                } else {
+                    // Sadness, they gave a number, but it's incorrect 😭
+                    answerCorrect = false
+                }
+            }, label: {
+                Text("Check Answer")
+                    .font(.largeTitle)
+            })
+                .padding()
+                .buttonStyle(.bordered)
+                .opacity(answerChecked == false ? 1.0 : 0.0)
+            Spacer()
+        }
+        
+        
+        
+            
+            
+        Button(action: {
+            subtrahend = Int.random(in: 1...12)
+            minuend = Int.random(in: 1...12)
+            
+            answerChecked = false
+            answerChecked = false
+            
+            
+            //reset field where the user givens an asnwer
+            //given = empty string
+            inputGiven = ""
+        }, label: {
+            
+            Text("New Question")
+                .font(.largeTitle)
+            
+        })
+        }
+            .padding()
+            .buttonStyle(.bordered)
+            .opacity(answerChecked == true ? 1.0 : 0.0)
+        
+        
+            .padding(.horizontal)
+            .font(.system(size: 72))
+        
+        
+        
+        
+    }
+}
+
+struct Subtraction_Previews: PreviewProvider {
+    static var previews: some View {
+        SubtractionView()
+    }
+}
